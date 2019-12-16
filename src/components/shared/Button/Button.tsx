6f4ce42f26189ab46fef1ff87f56a23445809d81
Solona Armstrong <solona.armstrong@xapo.com>
2019-12-16 05:11:56 -0800

    Add card component

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { COLORS } from "../../constants/colors";
+import { COLORS } from "../../../constants/colors";
 
 interface ButtonProps {
   children: string;
